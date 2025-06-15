34eb3066fd1c9cc2ece5eb498bae709a42c6ba09
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-06-15 14:33:42 +0900

    feat: 데이터 및 타입 정의 추가

---
+++ src/utils/data.ts
@@ -0,0 +1,97 @@
+// 인포 - info
+export const info = {
+  groom: {
+    nameKo: "김지훈",
+    nameEn: "Kim Jihoon",
+    groomFather: "김동해",
+    groomMother: "이향란",
+  },
+  bride: {
+    nameKo: "김아람",
+    nameEn: "Kim Ahram",
+    brideFather: "김종태",
+    brideMother: "최미숙",
+  },
+  images: {
+    main: "/image/subImage2.png",
+    sub1: "/image/subimage1.jpg",
+    sub2: "/image/subImage2.png",
+  },
+  wedding: {
+    date: "2025-09-06",
+    day: "토요일",
+    time: "오전 11시 30분",
+    txt: "구월의\n여섯번째 날.",
+    location: "국방컨벤션 1층 에메랄드홀",
+    address: "서울 용산구 이태원로 22",
+    numOfBlank: "1",
+  },
+  message: {
+    footer: "Jihoon ❤️ Ahram\n 저희 행복하게 잘 살아가겠습니다.",
+    quote:
+      "꽃이 피었다고 너에게 쓰고 \n꽃이 졌다고 너에게 쓴다. \n\n너에게 쓴 마음이 \n벌써 길이 되었다. \n\n너에게 쓴 마음이 \n벌써 내 일생이 되었다. \n\n천양희 <너에게 쓴다>",
+  },
+};
+
+// 연락처 - contacts
+export const contacts = [
+  {
+    role: "신랑",
+    name: "김지훈",
+    phone: "010-9730-9736",
+  },
+  {
+    role: "신부",
+    name: "김아람",
+    phone: "010-4687-4654",
+  },
+  {
+    role: "신랑 아버지",
+    name: "김동해",
+    phone: "010-3456-7890",
+  },
+  {
+    role: "신부 아버지",
+    name: "김종태",
+    phone: "010-9783-4654",
+  },
+  {
+    role: "신랑 어머니",
+    name: "이향란",
+    phone: "010-5678-9012",
+  },
+  {
+    role: "신부 어머니",
+    name: "최미숙",
+    phone: "010-4877-4655",
+  },
+];
+
+// 갤러리 - gallery
+export const gallery = [
+  "https://images.unsplash.com/photo-1513002749550-c59d786b8e6c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1590372648787-fa5a935c2c40?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1513002749550-c59d786b8e6c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1590372648787-fa5a935c2c40?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1513002749550-c59d786b8e6c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1590372648787-fa5a935c2c40?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1513002749550-c59d786b8e6c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1590372648787-fa5a935c2c40?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1513002749550-c59d786b8e6c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1590372648787-fa5a935c2c40?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1513002749550-c59d786b8e6c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1590372648787-fa5a935c2c40?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1590372648787-fa5a935c2c40?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1513002749550-c59d786b8e6c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1590372648787-fa5a935c2c40?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+// 계좌번호 - accounts
+export const accounts = [
+  { type: "groom", name: "김지훈", account: "국민 0123-45-6789101" },
+  { type: "groom", name: "김동해", account: "국민 0123-45-123123" },
+  { type: "groom", name: "이향란", account: "국민 0123-45-6789101" },
+  { type: "bride", name: "김아람", account: "국민 0123-45-6789101" },
+  { type: "bride", name: "김종태", account: "국민 0123-45-123123" },
+  { type: "bride", name: "최미숙", account: "국민 0123-45-6789101" },
+];
---
+++ src/utils/types.ts
@@ -0,0 +1,62 @@
+// 인포 - info
+export type MainInfo = {
+  groom: {
+    nameKo: string;
+    nameEn: string;
+    groomFather: string;
+    groomMother: string;
+  };
+  bride: {
+    nameKo: string;
+    nameEn: string;
+    brideFather: string;
+    brideMother: string;
+  };
+  images: {
+    main: string;
+    sub1: string;
+    sub2: string;
+  };
+  wedding: {
+    date: string;
+    day: string;
+    time: string;
+    txt: string;
+    location: string;
+    address: string;
+    numOfBlank: string;
+  };
+  message: {
+    footer: string;
+    quote: string;
+  };
+};
+
+export interface IMainInfo {
+  info: MainInfo;
+}
+
+// 연락처 - contacts
+export type Contact = {
+  role: string;
+  name: string;
+  phone: string;
+};
+
+export interface IContact {
+  contact: Contact[];
+}
+
+// 갤러리 - gallery
+export type Gallery = string[];
+
+// 계좌번호 - accounts
+export type Account = {
+  type: string;
+  name: string;
+  account: string;
+};
+
+export interface IAccount {
+  acc: Account[];
+}
